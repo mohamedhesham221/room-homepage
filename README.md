c86dbed041be1421d78c6ec2748bcd6043891dac
mohamedhesham221 <mohmadetsh874@gmail.com>
2023-02-08 14:56:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Frontend Mentor - Room homepage solution
+
+This is a solution to the [Room homepage challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/room-homepage-BtdBY_ENq). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Navigate the slider using either their mouse/trackpad or keyboard
+
+### Screenshot
+
+![screenshot](./dist/images/screencapture.png)
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Javascript
+
+### What I learned
+
+I learned how to create basic images slider with plain javascript.
+
+## Author
+
+- Frontend Mentor - [@mohamedhesham221](https://www.frontendmentor.io/profile/mohamedhesham221)
+- Linkedin - [Muhammad Hisham](https://www.linkedin.com/in/muhammad-hisham-23544b253/)
---
+++ dist/main.js
@@ -0,0 +1,76 @@
+let closeNav = document.getElementById("close-nav"),
+  openNav = document.getElementById("open-nav"),
+  navbar = document.getElementById("navbar"),
+  img = document.getElementById("slider-img"),
+  head = document.getElementById("slider-head"),
+  btnLeft = document.getElementById("btn-left"),
+  btnRight = document.getElementById("btn-right"),
+  desc = document.getElementById("slider-desc"),
+  count = 1,
+  slider = [
+    {
+      sliderHead: "Discover innovative ways to decorate",
+      sliderDesc: `We provide unmatched quality, comfort, and style for property owners across the country. Our
+      experts combine form and function in bringing
+      your vision to life. Create a room in your own style
+      with our collection and make your property a reflection of you and what you love.`,
+    },
+    {
+      sliderHead: "We are available all across the globe",
+      sliderDesc: `With stores all over the world, it's easy for you to find furniture for your home or place of business. 
+      Locally, we're in most major cities throughout the country. Find the branch nearest you using our 
+      store locator. Any questions? Don't hesitate to contact us today.`,
+    },
+    {
+      sliderHead: "Manufactured with the best materials",
+      sliderDesc: `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
+      to ensure that every product is made as perfect and as consistent as possible. With three decades of 
+      experience in this industry, we understand what customers want for their home and office.`,
+    },
+  ];
+
+  if (window.matchMedia("(min-width: 600px)").matches) {
+    img.src = "../dist/images/desktop-image-hero-"+count+".jpg"
+  } else {
+    img.src = "../dist/images/mobile-image-hero-"+count+".jpg"
+  }
+  
+const goRight = () => {
+  count++;
+  if (count == 4) {
+    count = 1;
+  }
+  if (window.matchMedia("(min-width: 600px)").matches) {
+    img.src = "../dist/images/desktop-image-hero-"+count+".jpg"
+  } else {
+    img.src = "../dist/images/mobile-image-hero-"+count+".jpg"
+  }
+  head.textContent = slider[count - 1].sliderHead;
+  desc.textContent = slider[count - 1].sliderDesc;
+};
+const goLeft = () => {
+  count--;
+  if (count == 0) {
+    count = 3;
+  }
+  if (window.matchMedia("(min-width: 600px)").matches) {
+    img.src = "../dist/images/desktop-image-hero-"+count+".jpg"
+  } else {
+    img.src = "../dist/images/mobile-image-hero-"+count+".jpg"
+  }
+  head.textContent = slider[count - 1].sliderHead;
+  desc.textContent = slider[count - 1].sliderDesc;
+};
+btnRight.addEventListener("click", goRight);
+btnLeft.addEventListener("click", goLeft);
+
+const showNav = () => {
+  navbar.classList.remove("hide-nav");
+  navbar.classList.add("show-nav");
+};
+const hideNav = () => {
+  navbar.classList.remove("show-nav");
+  navbar.classList.add("hide-nav");
+};
+closeNav.addEventListener("click", hideNav);
+openNav.addEventListener("click", showNav);
